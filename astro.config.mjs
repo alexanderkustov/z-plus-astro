0d0350af439507f3f07b6baec488965f1a31c0dd
Alex K <knuxus@gmail.com>
2026-02-04 17:54:30 +0000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 const isGitHubPages = Boolean(process.env.GITHUB_PAGES);
 
@@ -7,5 +8,5 @@ export default defineConfig({
   output: "static",
   base: "",
   site: "https://z-plus.co.uk",
-  integrations: [react()]
+  integrations: [react(), sitemap({})]
 });
