7f7dbe2f764b95eee96977aae8fcd68962de81cc
Alex K <knuxus@gmail.com>
2026-02-04 12:12:31 +0000

    z-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
 const isGitHubPages = Boolean(process.env.GITHUB_PAGES);
 
 export default defineConfig({
   output: "static",
-  base: isGitHubPages && repoName ? `/${repoName}` : "",
-  site: process.env.SITE ?? "https://example.com",
+  base: "",
+  site: "https://z-plus.co.uk",
   integrations: [react()]
 });
